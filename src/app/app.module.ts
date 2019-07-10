9aef253efdc69d817ea140fb3cb58d3ef30c30b2
huangkai <18778335525@163.com>
2019-07-10 21:23:25 +0800

    添加路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PingComponent } from './pages/ping/ping.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { PingComponent } from './pages/ping/ping.component';
     PingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
